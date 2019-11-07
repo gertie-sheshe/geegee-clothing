0df64b92957e34ed844aa77db77d5c63a82deb32
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2019-11-07 16:07:41 +0300

    initial cleanup and first component

---
+++ src/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+)
