0b6405241bf1d4238eae44dd85e4b536defddc69
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2019-11-11 10:35:47 +0300

    Directory component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,43 +1,12 @@
 import React from 'react';
 
-import MenuItem from '../../components/menu-item/menu-item.component';
+import Directory from '../../components/directory/directory.component';
 
 import './homepage.styles.scss';
 
 const HomePage = () => (
     <div className='homepage'>
-        <div className='directory-menu'>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>LEASH</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>TOYS</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>TREATS</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>SHAMPOO</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>BEDS</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-        </div>
+        <Directory />
     </div>
 );
 
