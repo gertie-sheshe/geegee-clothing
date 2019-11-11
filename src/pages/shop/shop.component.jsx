c2273f2fdfb28a165b69db050ad62d160fa47827
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2019-11-11 13:25:52 +0300

    add shop page + more routing config

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SHOP_DATA from './shop-data';
+
+class ShopPage extends React.Component {
+    state = SHOP_DATA;
+
+    render() {
+        return(
+            <div>SHOP PAGE</div>
+        )
+    }
+}
+
+export default ShopPage;
