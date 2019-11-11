1a151af5ec9b99786d4bd1e1e0c9582fa7cf9e46
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2019-11-11 12:29:50 +0300

    integrate react router navigation

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,33 +13,33 @@ class Directory extends React.Component {
             title: 'toys',
             imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
             id: 1,
-            linkUrl: 'shop/hats'
+            linkUrl: 'shop/toys'
         },
         {
-        title: 'clothing',
-        imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-        id: 2,
-        linkUrl: 'shop/jackets'
+            title: 'clothing',
+            imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+            id: 2,
+            linkUrl: 'shop/clothing'
         },
         {
             title: 'treats',
             imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
             id: 3,
-            linkUrl: 'shop/sneakers'
+            linkUrl: 'shop/treats'
         },
         {
             title: 'leash',
             imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
             size: 'large',
             id: 4,
-            linkUrl: 'shop/womens'
+            linkUrl: 'shop/leashs'
         },
         {
             title: 'crates',
             imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
             size: 'large',
             id: 5,
-            linkUrl: 'shop/mens'
+            linkUrl: 'shop/crates'
         }]
     };
 
@@ -47,8 +47,8 @@ class Directory extends React.Component {
     {
         return(
             <div className="directory-menu">
-                {this.state.sections.map((section) => (
-                    <MenuItem key={section.id} {...section} />
+                {this.state.sections.map(({id, ...section}) => (
+                    <MenuItem key={id} {...section} />
                 ))}
             </div>
         )
