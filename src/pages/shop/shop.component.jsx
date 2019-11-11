1a46080d5559da86500f90b07dff5523ec15917f
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2019-11-11 16:24:32 +0300

    renaming

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import CollectionPreview from '../../components/preview-collection/collection-preview.component';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 import SHOP_DATA from './shop-data';
 
 class ShopPage extends React.Component {
