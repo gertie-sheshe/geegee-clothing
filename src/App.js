bf2ee8ece48ff120b6d707e074f114055b77b62d
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2019-11-12 10:51:16 +0300

    auth and sign in components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Switch, Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import Auth from './pages/auth/auth.component';
 
 import './App.css';
 
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={Auth} />
       </Switch>
     </div>
   );
