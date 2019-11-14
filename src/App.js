618bb439091471d8491003d747c335916abbf8d9
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2019-11-14 13:44:12 +0300

    save user data in firebase

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import Auth from './pages/auth/auth.component';
 
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 import './App.css';
 
@@ -21,10 +21,8 @@ class App extends React.Component {
   
   componentDidMount() {
     // user session persisted via firebase
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({
-        currentUser: user
-      });
+    auth.onAuthStateChanged(async user => {
+      const savedUser = await createUserProfileDocument(user);
     });
   }
 
