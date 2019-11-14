dcc1ae32f9754d1992daf46eb8061eef6447a451
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2019-11-14 15:42:57 +0300

    add sign up component

--- src/pages/auth/auth.component.jsx
+++ src/pages/auth/auth.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in-component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 import './auth.styles.scss';
 
 const Auth = () => (
     <div className = 'sign-in-and-sign-up'>
         <SignIn />
+        <SignUp />
     </div>
 
 )
