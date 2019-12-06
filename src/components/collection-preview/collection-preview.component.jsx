490f1977b397d4d9a4657c49436d8744e45f840b
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2019-12-06 11:28:18 +0300

    destructure error

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -7,10 +7,10 @@ const CollectionPreview = ({title, items}) => (
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
-            {
+            {   
                 items
                 .filter((item, index) => index < 4)
-                .map(({item}) => (
+                .map((item) => (
                     <CollectionItem key={item.id} item={item}/>
                 ))
             }
