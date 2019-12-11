a67daa182e7dc1c014d8cdd26e35b080c110eced
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2019-12-11 15:09:42 +0300

    toggle cart action in cart dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,18 +6,23 @@ import { withRouter } from 'react-router-dom';
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 
+import {toggleCartHidden} from '../../redux/cart/cart.actions';
+
 // Selectors
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropDown = ({ cartItems, history }) => (
+const CartDropDown = ({ cartItems, history, dispatch}) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             { cartItems.length ? cartItems.map( cartItem => <CartItem key={cartItem.id} item={cartItem}/>) :
         <span className='empty-message'>Your cart is empty</span>}
         </div>
-        <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={
+            () => {history.push('/checkout');
+            dispatch(toggleCartHidden());
+            }}>GO TO CHECKOUT</CustomButton>
     </div>
 );
 
