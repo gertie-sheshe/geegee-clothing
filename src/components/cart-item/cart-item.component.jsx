8cc853868433c5b997be946cef37932d6a4d053f
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2020-01-02 11:49:23 +0300

    cart item styled components

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 
 import './cart-item.styles.scss';
 
-const CartItem = ({ item: { imageUrl, price, name, quantity}}) => (
-    <div className='cart-item'>
-        <img src={imageUrl} alt='item in cart' />
-        <div className='item-details'>
-            <span className='name'>{name}</span>
-            <span className='price'>${price} x {quantity}</span>
-        </div>
-    </div>
-)
+import {
+  CartItemContainer,
+  CartItemImage,
+  ItemDetailsContainer,
+} from './cart-item.styles';
+
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
+  <CartItemContainer>
+    <CartItemImage src={imageUrl} alt="item in cart" />
+    <ItemDetailsContainer>
+      <span>{name}</span>
+      <span>
+        ${price} x {quantity}
+      </span>
+    </ItemDetailsContainer>
+  </CartItemContainer>
+);
 
 export default CartItem;
