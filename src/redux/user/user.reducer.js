8b3aed23be430040607736c898887aced82f074a
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2020-01-23 18:22:02 +0300

    auth types and reducer changes

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,18 +1,27 @@
-import {userActionTypes} from './user.types';
+import { userActionTypes } from './user.types';
 
 const INITIAL_STATE = {
-    currentUser: null
-}
+  currentUser: null,
+  error: null,
+};
 const userReducer = (state = INITIAL_STATE, action) => {
-    switch(action.type) {
-        case userActionTypes.SET_CURRENT_USER :
-            return {
-              ...state,
-              currentUser: action.payload,
-            };
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    case userActionTypes.GOOGLE_SIGN_IN_SUCCESS:
+    case userActionTypes.EMAIL_SIGN_IN_SUCCESS:
+      return {
+        ...state,
+        currentUser: action.payload,
+        error: null,
+      };
+    case userActionTypes.GOOGLE_SIGN_IN_FAILURE:
+    case userActionTypes.EMAIL_SIGN_IN_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
 export default userReducer;
