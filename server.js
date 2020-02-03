91c71dcac10fa9690cd0fef67175d8eadc01d8fa
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2020-02-03 18:27:08 +0300

    setup server

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.listen(port, err => {
+  if (err) throw error;
+
+  console.log(`Running on port ${port}`);
+});
