37d95c52725d9963f8b81e3c79adf40060a18e67
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2020-02-24 12:44:35 +0300

    make our app pwa compliant, added service-worker

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
 
+import * as serviceWorker from './serviceWorker';
+
 import './index.css';
 import App from './App';
 
@@ -12,9 +14,11 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <PersistGate persistor={persistor}>
-          <App />
+        <App />
       </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
 );
+
+serviceWorker.register();
